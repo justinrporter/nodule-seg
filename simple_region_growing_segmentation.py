f6acf944ab489cf935e7e3ef3f8e56b0805cd001
Justin Porter <justinrporter@gmail.com>
2015-06-11 16:01:47 -0500

    Mostly functional srgs.py

--- simple_region_growing_segmentation.py
+++ simple_region_growing_segmentation.py
@@ -8,6 +8,8 @@ import sys
 import argparse
 import itk
 
+IMG_UC = itk.Image[itk.UC, 3]  # pylint: disable=no-member
+IMG_F = itk.Image[itk.F, 3]  # pylint: disable=no-member
 
 def process_command_line(argv):
     '''Parse the command line and do a first-pass on processing them into a
@@ -60,29 +62,39 @@ def process_command_line(argv):
 def attach_smooth(pipe, iterations, timestep):
     '''Attach a CurvatureFlowImageFilter to the output of the given
     filter stack.'''
-    # pylint: disable=no-member
-    image_type = itk.Image[itk.UC, 3]
+    # pylint: disable=no-name-in-module,no-member
+    from itk import CurvatureFlowImageFilter
 
-    cfif = itk.ConfidenceConnectedImageFilter[image_type, image_type].New()
+    cfif = CurvatureFlowImageFilter[IMG_F, IMG_F].New()
 
     cfif.SetNumberOfIterations(iterations)
-    cfif.SetTimestep(timestep)
+    cfif.SetTimeStep(timestep)
 
-    cfif.setInput(pipe.GetOutput())
-
-    if DEBUG:
-        cfif.Update()
+    cfif.SetInput(pipe.GetOutput())
 
     return cfif
+
+
+def attach_converter(pipe, type_in, type_out):
+    '''Attach a CastImageFilter to convert from one itk image type to
+    another.'''
+    # pylint: disable=no-name-in-module
+    from itk import CastImageFilter
+
+    conv = CastImageFilter[type_in, type_out].New()
+
+    conv.SetInput(pipe.GetOutput())
+
+    return conv
 
 
 def attach_connect(pipe, iterations, stddevs, neighborhood, seed):
     '''Attach a ConfidenceConnectedImageFilter to the output of the given
     filter stack.'''
-    # pylint: disable=no-member
-    image_type = itk.Image[itk.UC, 3]
+    # pylint: disable=no-name-in-module, no-member
+    from itk import ConfidenceConnectedImageFilter
 
-    ccif = itk.ConfidenceConnectedImageFilter[image_type, image_type].New()
+    ccif = ConfidenceConnectedImageFilter[IMG_UC, IMG_UC].New()
 
     ccif.AddSeed(seed)
     ccif.SetNumberOfIterations(iterations)
@@ -91,25 +103,19 @@ def attach_connect(pipe, iterations, stddevs, neighborhood, seed):
 
     ccif.SetInput(pipe.GetOutput())
 
-    if DEBUG:
-        ccif.Update()
-
     return ccif
 
 
 def get_reader(fname):
     '''Initialize a filter pipeline by building an ImageFileReader based on
     the given file 'fname'.'''
-    # pylint: disable=no-member
-    image_type = itk.Image[itk.UC, 3]
+    from itk import ImageFileReader  # pylint: disable=no-name-in-module
 
-    reader = itk.ImageFileReader[image_type].New()
+    reader = ImageFileReader[IMG_F].New()
 
     reader.SetFileName(fname)
 
-    if DEBUG:
-        reader.SetDebug(True)
-        reader.Update()
+    reader.Update()
 
     return reader
 
@@ -117,16 +123,12 @@ def get_reader(fname):
 def attach_writer(pipe, fname):
     '''Initialize and attach an ImageFileWriter to the end of a filter pipeline
     to write out the result.'''
-    # pylint: disable=no-member
-    image_type = itk.Image[itk.UC, 3]
+    from itk import ImageFileWriter  # pylint: disable=no-name-in-module
 
-    writer = itk.ImageFileWriter[image_type].New()
+    writer = ImageFileWriter[IMG_UC].New()
 
     writer.SetInput(pipe.GetOutput())
     writer.SetFileName(fname)
-
-    if DEBUG:
-        writer.Update()
 
     return writer
 
@@ -134,6 +136,8 @@ def attach_writer(pipe, fname):
 def main(argv=None):
     '''Run the driver script for this module. This code only runs if we're
     being run as a script. Otherwise, it's silent and just exposes methods.'''
+    # pylint: disable=no-member
+
     args = process_command_line(argv)
 
     pipe = get_reader(args.image)
@@ -143,9 +147,11 @@ def main(argv=None):
                           args.connect_stddevs,
                           args.connect_neighborhood,
                           args.seed)
+    pipe = attach_converter(pipe, IMG_UC, IMG_F)
     pipe = attach_smooth(pipe,
                          args.smooth_iterations,
                          args.smooth_timestep)
+    pipe = attach_converter(pipe, IMG_F, IMG_UC)
 
     pipe = attach_writer(pipe, args.output)
 
