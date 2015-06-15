b49e8604c9b4c8e8e892ffe27035c4fc08805a6b
Justin Porter <justinrporter@gmail.com>
2015-06-15 10:41:05 -0500

    Update srgs to have faster load time.

--- simple_region_growing_segmentation.py
+++ simple_region_growing_segmentation.py
@@ -6,32 +6,44 @@ availiable through ITK.'''
 
 import sys
 import argparse
-import itk
 
-IMG_UC = itk.Image[itk.UC, 3]  # pylint: disable=no-member
-IMG_F = itk.Image[itk.F, 3]  # pylint: disable=no-member
+
+def IMG_UC():  # pylint: disable=invalid-name
+    '''dynamically load unsigned character 3d image type to preven super long
+    loads when loading the script.'''
+    from itk import UC, Image  # pylint: disable=no-name-in-module
+    return Image[UC, 3]
+
+
+def IMG_F():  # pylint: disable=invalid-name
+    '''dynamically load 3d float image type to preven super long loads when
+    loading the script.'''
+    from itk import F, Image  # pylint: disable=no-name-in-module
+    return Image[F, 3]
+
 
 def process_command_line(argv):
     '''Parse the command line and do a first-pass on processing them into a
     format appropriate for the rest of the script.'''
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.
+                                     ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("image", nargs=1,
                         help="The image that should be segmented.")
-    parser.add_argument("--connect_iterations", nargs=1, type=int, default=10,
+    parser.add_argument("--connect_iterations", type=int, default=10,
                         help="The number of iterations to run" +
                              " ConfidenceConnectedImageFilter")
-    parser.add_argument('--connect_stddevs', nargs=1, type=float, default=2.0,
+    parser.add_argument('--connect_stddevs', type=float, default=2.0,
                         help="The number of voxel property standard devs " +
                         "to consider as connected.")
-    parser.add_argument('--connect_neighborhood', nargs=1, type=int, default=1,
+    parser.add_argument('--connect_neighborhood', type=int, default=1,
                         help='the number of local pixels around the seed to ' +
                         'use as the start of the calculation.')
-    parser.add_argument("--smooth_iterations", nargs=1, default=10, type=int,
+    parser.add_argument("--smooth_iterations", default=10, type=long,
                         help="The number of iterations to run" +
                              " CurvatureFlowImageFilter")
-    parser.add_argument('--smooth_timestep', nargs=1, default=0.01, type=float,
+    parser.add_argument('--smooth_timestep', default=0.01, type=float,
                         help="The step size used by CurvatureFlowImageFilter")
     parser.add_argument('--seed', nargs=3, type=int,
                         help="An initial point (in voxel ids) at a 'hint' as" +
@@ -52,9 +64,9 @@ def process_command_line(argv):
         ext = args.image[args.image.rfind('.'):]
         args.output = args.image.rstrip(ext) + "-label" + ext
 
-    # pylint: disable=global-variable-undefined
-    global DEBUG
-    DEBUG = args.debug
+    print "Running with settings:"
+    "\n".join([str(key)+": "+str(args.__getattribute__(key))
+               for key in vars(args)])
 
     return args
 
@@ -65,12 +77,13 @@ def attach_smooth(pipe, iterations, timestep):
     # pylint: disable=no-name-in-module,no-member
     from itk import CurvatureFlowImageFilter
 
-    cfif = CurvatureFlowImageFilter[IMG_F, IMG_F].New()
+    cfif = CurvatureFlowImageFilter[IMG_F(), IMG_F()].New()
 
     cfif.SetNumberOfIterations(iterations)
     cfif.SetTimeStep(timestep)
 
     cfif.SetInput(pipe.GetOutput())
+    cfif.Update()
 
     return cfif
 
@@ -84,6 +97,7 @@ def attach_converter(pipe, type_in, type_out):
     conv = CastImageFilter[type_in, type_out].New()
 
     conv.SetInput(pipe.GetOutput())
+    conv.Update()
 
     return conv
 
@@ -94,7 +108,7 @@ def attach_connect(pipe, iterations, stddevs, neighborhood, seed):
     # pylint: disable=no-name-in-module, no-member
     from itk import ConfidenceConnectedImageFilter
 
-    ccif = ConfidenceConnectedImageFilter[IMG_UC, IMG_UC].New()
+    ccif = ConfidenceConnectedImageFilter[IMG_F(), IMG_UC()].New()
 
     ccif.AddSeed(seed)
     ccif.SetNumberOfIterations(iterations)
@@ -102,6 +116,8 @@ def attach_connect(pipe, iterations, stddevs, neighborhood, seed):
     ccif.SetInitialNeighborhoodRadius(neighborhood)
 
     ccif.SetInput(pipe.GetOutput())
+
+    ccif.Update()
 
     return ccif
 
@@ -111,7 +127,7 @@ def get_reader(fname):
     the given file 'fname'.'''
     from itk import ImageFileReader  # pylint: disable=no-name-in-module
 
-    reader = ImageFileReader[IMG_F].New()
+    reader = ImageFileReader[IMG_F()].New()
 
     reader.SetFileName(fname)
 
@@ -125,7 +141,7 @@ def attach_writer(pipe, fname):
     to write out the result.'''
     from itk import ImageFileWriter  # pylint: disable=no-name-in-module
 
-    writer = ImageFileWriter[IMG_UC].New()
+    writer = ImageFileWriter[IMG_UC()].New()
 
     writer.SetInput(pipe.GetOutput())
     writer.SetFileName(fname)
@@ -142,17 +158,15 @@ def main(argv=None):
 
     pipe = get_reader(args.image)
 
+    pipe = attach_smooth(pipe,
+                         args.smooth_iterations,
+                         args.smooth_timestep)
+
     pipe = attach_connect(pipe,
                           args.connect_iterations,
                           args.connect_stddevs,
                           args.connect_neighborhood,
                           args.seed)
-    pipe = attach_converter(pipe, IMG_UC, IMG_F)
-    pipe = attach_smooth(pipe,
-                         args.smooth_iterations,
-                         args.smooth_timestep)
-    pipe = attach_converter(pipe, IMG_F, IMG_UC)
-
     pipe = attach_writer(pipe, args.output)
 
     # run the pipeline
