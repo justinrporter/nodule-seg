c0842fff586d0328c5c463ae269a92c96c6f84b4
Justin Porter <justinrporter@gmail.com>
2015-06-16 13:30:05 -0500

    Continue tuning geodesic segmentation strategy.

--- geodesic_segment.py
+++ geodesic_segment.py
@@ -23,18 +23,21 @@ def process_command_line(argv):
     parser.add_argument('--sigma', default=1.0, type=float,
                         help="The stddev in units of image spacing for the " +
                              "GradientMagnitudeRecursiveGaussianImageFilter.")
-    parser.add_argument('--alpha', default=1.0, type=float,
+    parser.add_argument('--alpha', default=-0.5, type=float,
                         help="Alpha ('A') parameter in sigmoid filter. Obeys" +
                         " the expression exp((-x+B)/A). Gain.")
-    parser.add_argument('--beta', default=0.0, type=float,
+    parser.add_argument('--beta', default=10, type=float,
                         help="Beta ('B') parameter in sigmoid filter. Obeys" +
                         " the expression exp((-x+B)/A). Zero adjustment.")
-    parser.add_argument('--propagation_scaling', default=1.0,
+    parser.add_argument('--propagation_scaling', default=5.0, type=float,
                         help="The weight on propagation force in level set " +
                         "segmentation.")
     parser.add_argument('--geodesic_iterations', default=10, type=int,
                         help="The number of iterations by the " +
                         "GeodesicActiveContourLevelSetImageFilter")
+    parser.add_argument('--seed_distance', default=5, type=int,
+                        help="The expected distance from the seed to the" +
+                        "first level set.")
 
     args = parser.parse_args(argv[1:])
 
@@ -52,21 +55,30 @@ def process_command_line(argv):
 def main(argv=None):
     '''Run the driver script for this module. This code only runs if we're
     being run as a script. Otherwise, it's silent and just exposes methods.'''
-    # pylint: disable=no-member
 
     args = process_command_line(argv)
 
     pipe = itk_attach.FileReader(args.image)
-    pipe = itk_attach.AnisoDiffStage(pipe)
-    pipe = itk_attach.GradMagRecGaussStage(pipe, args.sigma)
-    pipe = itk_attach.SigmoidStage(pipe, args.alpha, args.beta)
+    aniso = itk_attach.AnisoDiffStage(pipe)
+    gauss = itk_attach.GradMagRecGaussStage(aniso, args.sigma)
+    sigmo = itk_attach.SigmoidStage(gauss, args.alpha, args.beta)
 
-    pipe2 = itk_attach.FileReader(args.image)
-    pipe2 = itk_attach.FastMarchingStage(pipe2)
+    fastmarch = itk_attach.FastMarchingStage(pipe,
+                                             imageless=True,
+                                             seeds=[args.seed],
+                                             seed_value=args.seed_distance)
 
-    pipe = itk_attach.GeoContourLSetStage(pipe2, pipe,
+    pipe = itk_attach.GeoContourLSetStage(fastmarch, sigmo,
                                           args.propagation_scaling,
                                           args.geodesic_iterations)
+
+    itk_attach.FileWriter(aniso, 'out-aniso.nii').execute()
+    itk_attach.FileWriter(gauss, 'out-gauss.nii').execute()
+    itk_attach.FileWriter(sigmo, 'out-sigmo.nii').execute()
+
+    # pipe = itk_attach.BinaryThreshStage(pipe)
+
+    pipe = itk_attach.FileWriter(pipe, args.output)
 
     # run the pipeline
     pipe.execute()
