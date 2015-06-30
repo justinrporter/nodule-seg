0df7da939a4a01b72e62f4f9de74b145761f08b9
Justin Porter <justinrporter@gmail.com>
2015-06-30 16:29:49 -0500

    Add multiple thresholding option to union.py

--- union.py
+++ union.py
@@ -1,7 +1,8 @@
 import sys
 import argparse
 import os
-import re
+import medpy.io
+import compare_segmentations
 
 
 def process_command_line(argv):
@@ -15,13 +16,19 @@ def process_command_line(argv):
                         help="The images the script operates on.")
     parser.add_argument("--path", default=os.getcwd(),
                         help="Output path")
+    parser.add_argument("--outpath", default=None,
+                        help="The path to place the output images")
+    parser.add_argument("--thresholds", nargs="+", default=[2.0/3], type=float,
+                        help="A list of agreement thresholds to try")
 
     args = parser.parse_args(argv[1:])
+
+    print args
 
     return args
 
 
-def compute_union(images):
+def compute_union(images, threshold):
     import numpy as np
 
     consensus = np.zeros(images[0].shape)
@@ -35,17 +42,48 @@ def compute_union(images):
     if n_img == 0:
         raise ValueError("No image in had an acceptable size.")
 
-    consensus = consensus >= (2/3.0) * n_img
+    consensus = consensus >= (threshold * n_img)
 
     if not (1e3 < np.count_nonzero(consensus) < 1e7):
-        raise ValueError("Consensus image had unacceptable size")
+        print consensus.shape
+        raise ValueError("Consensus image had unacceptable size " +
+                         str(np.count_nonzero(consensus)))
 
     return consensus
 
 
-def outname(inname):
+def label_swap(inname, new_label="consensus"):
     ext = inname[inname.rfind('.'):]
-    return inname[:inname.rfind('-')]+"-consensus"+ext
+    return inname[:inname.rfind('-')]+"-"+new_label+ext
+
+
+def build_image(images, headers, img_group, threshold):
+    try:
+        consensus = compute_union(images, threshold)
+    except ValueError as exc:
+        print exc, "images:", img_group
+        return
+
+    medpy.io.save(consensus,
+                  label_swap(img_group[0], "consensus"),
+                  headers[0])
+
+
+def run_thresholds(images, thresholds, manual_name):
+    results = {}
+
+    for threshold in thresholds:
+        # pylint: disable=W0612
+        (manual, hdr) = medpy.io.load(manual_name)
+
+        consensus = compute_union(images, threshold)
+
+        stats = compare_segmentations.segmentation_stats(consensus,
+                                                         manual)
+
+        results[threshold] = stats
+
+    return results
 
 
 def main(argv=None):
@@ -53,7 +91,6 @@ def main(argv=None):
     being run as a script. Otherwise, it's silent and just exposes methods.'''
     args = process_command_line(argv)
 
-    import medpy.io
     from os import listdir
     from os.path import isfile, join
 
@@ -63,18 +100,36 @@ def main(argv=None):
             abspath = os.path.join(args.path, f)
             img_groups.setdefault(f.split('-')[0], []).append(abspath)
 
+    results = {}
+
     for key in img_groups:
         (images, headers) = zip(*[medpy.io.load(fname) for fname
                                   in img_groups[key]])
 
-        try:
-            consensus = compute_union(images)
-        except ValueError as exc:
-            print exc, "images:", img_groups[key]
-            continue
+        arb_img_name = img_groups[key][0]
 
-        medpy.io.save(consensus, outname(img_groups[key][0]), headers[0])
-        print outname(img_groups[key][0])
+        if len(args.thresholds) == 1:
+            build_image(images, headers, img_groups[key], args.thresholds[0])
+            print label_swap(arb_img_name, "")
+        else:
+            try:
+                stats = run_thresholds(images,
+                                       args.thresholds,
+                                       label_swap(arb_img_name, "1-label"))
+            except ValueError:
+                print "failed at ", label_swap(arb_img_name, "")
+
+            results_key = arb_img_name.split('-')[0]
+            results_key = results_key[0:results_key.rfind('/')]
+            results[arb_img_name.split('-')[0]] = stats
+            print label_swap(arb_img_name, "")
+
+    if results:
+        import json
+        with open("union-results.json", 'w') as f:
+            json_out = json.dumps(results, sort_keys=True,
+                                  indent=4, separators=(',', ': '))
+            f.write(json_out)
 
     return 1
 
