72e73befbfcbbe2a891430bd1b3d839cd0ef5c48
Justin Porter <justinrporter@gmail.com>
2015-07-23 11:18:21 -0500

    Add debug flag to correct segmentation script list.

--- correct_segs.sh
+++ correct_segs.sh
@@ -1,8 +1,8 @@
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/416f542e2abdc057d1579ebff31aef5e2828d573.nii --seed 80 260 100 --media_root dedup_media_root --log dedup_media_root/log
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/6bb04fd0396c8dcf0fec632934fa3ce098ac8a9f.nii --seed 328 323 176 --media_root dedup_media_root --log dedup_media_root/log
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/85939246185326d43a9f67094ff9dbfd3d6b9767.nii --seed 318 297 146 --media_root dedup_media_root --log dedup_media_root/log
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/a4240efe5c38164a41cced3972275c0498da8868.nii --seed 409 310 56 --media_root dedup_media_root --log dedup_media_root/log
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/c83729cab7901b3adfb60fcfb0a221cf4daca30f.nii --seed 418 381 69 --media_root dedup_media_root --log dedup_media_root/log
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/d946ecfb09e059adaf194950c047a7b4fc1b1282.nii --seed 427 157 37 --media_root dedup_media_root --log dedup_media_root/log
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/a2188af9275f307f37c68bf040c3c45b6f42f37b.nii --seed 345 297 33 --media_root dedup_media_root --log dedup_media_root/log
-time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/98a1fbfda487074f9ba4ad14d3a9d776619741e6.nii --seed 163 361 126 --media_root dedup_media_root --log dedup_media_root/log
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/416f542e2abdc057d1579ebff31aef5e2828d573.nii --seed 80 260 100 --media_root dedup_media_root --log dedup_media_root/log --debug
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/6bb04fd0396c8dcf0fec632934fa3ce098ac8a9f.nii --seed 328 323 176 --media_root dedup_media_root --log dedup_media_root/log --debug
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/85939246185326d43a9f67094ff9dbfd3d6b9767.nii --seed 318 297 146 --media_root dedup_media_root --log dedup_media_root/log --debug
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/a4240efe5c38164a41cced3972275c0498da8868.nii --seed 409 310 56 --media_root dedup_media_root --log dedup_media_root/log --debug
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/c83729cab7901b3adfb60fcfb0a221cf4daca30f.nii --seed 418 381 69 --media_root dedup_media_root --log dedup_media_root/log --debug
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/d946ecfb09e059adaf194950c047a7b4fc1b1282.nii --seed 427 157 37 --media_root dedup_media_root --log dedup_media_root/log --debug
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/a2188af9275f307f37c68bf040c3c45b6f42f37b.nii --seed 345 297 33 --media_root dedup_media_root --log dedup_media_root/log --debug
+time python ./segment/masterseg.py --nseeds 1 dedup_media_root/init/98a1fbfda487074f9ba4ad14d3a9d776619741e6.nii --seed 163 361 126 --media_root dedup_media_root --log dedup_media_root/log --debug
